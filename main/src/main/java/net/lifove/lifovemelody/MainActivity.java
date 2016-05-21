69df5fab086c7d1e53332772c93c8fc8d1ad8e17
JC <jc@lifove.net>
2016-05-21 15:55:03 -0400

    Add hiding AppBar

--- main/src/main/java/net/lifove/lifovemelody/MainActivity.java
+++ main/src/main/java/net/lifove/lifovemelody/MainActivity.java
@@ -1,8 +1,17 @@
 package net.lifove.lifovemelody;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.SwitchCompat;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,16 +22,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
+    AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -100,12 +113,47 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            toolbar.setTitle("Genesis");
+            appBarLayout.animate().setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    // appBarLayout.setVisibility(AppBarLayout.INVISIBLE);
+                    CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams)appBarLayout.getLayoutParams();
+
+                    lp.height = 0;
+
+                    appBarLayout.setLayoutParams(lp);
+                }
+            });
+            appBarLayout.animate().translationY(-appBarLayout.getBottom()).setInterpolator(new AccelerateInterpolator()).start();
+
             // Handle the camera action
 
-            toolbar.setTitle("Genesis");
-            toolbar.animate().translationY(-toolbar.getBottom()).setInterpolator(new AccelerateInterpolator()).start();
+
+            //CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams)appBarLayout.getLayoutParams();
+
+            //lp.height = 0;
+
+            //appBarLayout.setLayoutParams(lp);
+
 
         } else if (id == R.id.nav_gallery) {
+
+            appBarLayout.animate().setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    super.onAnimationStart(animation);
+                    appBarLayout.setVisibility(AppBarLayout.VISIBLE);
+                    CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams)appBarLayout.getLayoutParams();
+
+                    lp.height = toolbar.getBottom();
+
+                    appBarLayout.setLayoutParams(lp);
+                }
+            });
+
+            appBarLayout.animate().translationY(0).setInterpolator(new AccelerateInterpolator()).start();
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -114,6 +162,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_fullscreen) {
 
         }
 
